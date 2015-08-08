cf8780fde028cc00817e23dad65852e99d649316
Dmitriy Yazykov <pro.d1mas91@gmail.com>
2015-08-08 14:15:43 +0400

    Шаблон главной страницы

---
+++ local/templates/yd2/components/bitrix/news.line/main_page_posts/template.php
@@ -0,0 +1,16 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+$this->setFrameMode(true);
+//dump($arResult);
+?>
+<div id="posts">
+    <?foreach($arResult["ITEMS"] as $arItem):?>
+        <?
+        $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+        $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+        ?>
+        <div id="<?=$this->GetEditAreaId($arItem['ID']);?>" class="post">
+            <h2 class="main_title"><a href="<?=$arItem["DETAIL_PAGE_URL"]?>"><?=$arItem["NAME"]?></a></h2>
+            <p class="post_date"><?=formatBDate($arItem["DATE_CREATE"])?></p>
+        </div>
+    <?endforeach;?>
+</div>
---
+++ local/templates/yd2/header.php
@@ -0,0 +1,102 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta property="og:image" content="http://ydmitriy.ru/img/webdev.png"/>
+    <meta property="og:title" content="<?$APPLICATION->ShowTitle()?>"/>
+    <meta property="og:url" content="http://ydmitriy.ru<?=$APPLICATION->GetCurPage()?>"/>
+    <meta property="og:description" content="<?$APPLICATION->ShowProperty("description")?>"/>
+    <meta name="p:domain_verify" content="ab7779b3d7f22e73c904eee707a9ae49"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?$APPLICATION->ShowTitle()?></title>
+    <link rel="icon" href="<?=SITE_TEMPLATE_PATH?>/img/favicon1.ico" type="image/vnd.microsoft.icon"/>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <?$APPLICATION->ShowHead();?>
+    <?IncludeTemplateLangFile(__FILE__);?>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+<body>
+<?$APPLICATION->ShowPanel();?>
+    <div id="menu">
+        <div>
+            <?$APPLICATION->IncludeComponent(
+                "bitrix:main.include",
+                "",
+                Array(
+                    "AREA_FILE_SHOW" => "file",
+                    "AREA_FILE_SUFFIX" => "inc",
+                    "EDIT_TEMPLATE" => "",
+                    "PATH" => "/menutitle.php"
+                )
+            );?>
+        </div>
+        <?$APPLICATION->IncludeComponent(
+	"bitrix:menu", 
+	"right_menu", 
+	array(
+		"ROOT_MENU_TYPE" => "top",
+		"MENU_CACHE_TYPE" => "Y",
+		"MENU_CACHE_TIME" => "36000",
+		"MENU_CACHE_USE_GROUPS" => "Y",
+		"MENU_CACHE_GET_VARS" => array(
+		),
+		"MAX_LEVEL" => "1",
+		"CHILD_MENU_TYPE" => "left",
+		"USE_EXT" => "N",
+		"DELAY" => "N",
+		"ALLOW_MULTI_SELECT" => "N",
+		"COMPONENT_TEMPLATE" => "right_menu"
+	),
+	false
+);?>
+        <div id="social">
+            <?$APPLICATION->IncludeComponent(
+                "bitrix:main.include",
+                "",
+                Array(
+                    "AREA_FILE_SHOW" => "file",
+                    "AREA_FILE_SUFFIX" => "inc",
+                    "EDIT_TEMPLATE" => "",
+                    "PATH" => "/social.php"
+                )
+            );?>
+        </div>
+        <div id="bx-composite-banner"></div>
+    </div>
+    <div id="workarea">
+        <div id="header">
+            <h1 class="name">
+                <span class="search_button">
+                    <noindex>
+                        <i class="fa fa-search"></i>
+                    </noindex>
+                </span>
+                <a href="/">
+                    Языков Дмитрий<br>
+                </a>
+                <span class="profession">
+                    Web-developer
+                </span>
+                <span class="button">
+                    <noindex><i class="fa fa-bars"></i></noindex>
+                </span>
+            </h1>
+            <div class="arrow">
+                <noindex>
+                    <a href="#content">
+                        &#9660;
+                    </a>
+                </noindex>
+            </div>
+        </div>
+        <noindex>
+        <div id="search_panel">
+            <div id="search_form">
+                <form action="/search">
+                    <input type="text" name="q" value="" placeholder="Хочу почитать про...">
+                    <input type="submit" value="Поиск">
+                </form>
+            </div>
+        </div>
+        </noindex>
+        <div id="content">
