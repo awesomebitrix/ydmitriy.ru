de7d158b8aabad258c415e5114d21cb9ddfaaa16
Dmitry Yazykov <work@ydmitriy.ru>
2015-10-28 17:39:56 +0300

    LF

--- local/templates/yd2page/footer.php
+++ local/templates/yd2page/footer.php
@@ -2,6 +2,7 @@
 
 </div>
 <?$APPLICATION->AddHeadScript("/bower_components/jquery/dist/jquery.js")?>
+<?$APPLICATION->AddHeadScript("https://google-code-prettify.googlecode.com/svn/loader/run_prettify.js?skin=desert")?>
 <?$APPLICATION->AddHeadScript("/js/my.js")?>
 <?$APPLICATION->IncludeComponent(
     "bitrix:main.include",
