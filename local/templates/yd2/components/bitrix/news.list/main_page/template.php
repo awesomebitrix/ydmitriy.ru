5f0c62923fffda9f89e991b9b57fb6c87fef54e1
Dmitry Yazykov <work@ydmitriy.ru>
2016-06-30 11:06:00 +0300

    Шаблон компонента news.list на главной странице

---
+++ local/templates/yd2/components/bitrix/news.list/main_page/template.php
@@ -0,0 +1,29 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+?>
+<div id="posts">
+    <?foreach($arResult["ITEMS"] as $arItem):?>
+        <?
+        $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+        $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+        ?>
+        <div id="<?=$this->GetEditAreaId($arItem['ID']);?>" class="post">
+            <h2 class="main_title"><a href="<?=$arItem["DETAIL_PAGE_URL"]?>"><?=$arItem["NAME"]?></a></h2>
+            <p class="post_date"><?=formatBDate($arItem["DATE_CREATE"])?></p>
+        </div>
+    <?endforeach;?>
+</div>
+<?if($arParams["DISPLAY_BOTTOM_PAGER"]):?>
+    <?=$arResult["NAV_STRING"]?>
+<?endif;?>
