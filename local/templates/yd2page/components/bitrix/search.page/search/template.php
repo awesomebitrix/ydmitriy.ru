23ab2f3e81b98e55283718e437b26b802627f1e4
Dmitry Yazykov <work@ydmitriy.ru>
2016-06-30 11:07:21 +0300

    Шаблон компонента search.page на странице поиска

---
+++ local/templates/yd2page/components/bitrix/search.page/search/template.php
@@ -0,0 +1,99 @@
+<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+?>
+<form action="" method="get">
+
+<!--    <div class="input">-->
+<!--	    <input type="text" name="q" value="--><?//=$arResult["REQUEST"]["QUERY"]?><!--"/>-->
+<!--        <input type="submit" value="--><?//=GetMessage("SEARCH_GO")?><!--" class="btn"/>-->
+<!--    </div>-->
+<!--	&nbsp;-->
+<!--	<input type="hidden" name="how" value="--><?//echo $arResult["REQUEST"]["HOW"]=="d"? "d": "r"?><!--" />-->
+<?if($arParams["SHOW_WHEN"]):?>
+	<script>
+	var switch_search_params = function()
+	{
+		var sp = document.getElementById('search_params');
+		var flag;
+		var i;
+
+		if(sp.style.display == 'none')
+		{
+			flag = false;
+			sp.style.display = 'block'
+		}
+		else
+		{
+			flag = true;
+			sp.style.display = 'none';
+		}
+
+		var from = document.getElementsByName('from');
+		for(i = 0; i < from.length; i++)
+			if(from[i].type.toLowerCase() == 'text')
+				from[i].disabled = flag;
+
+		var to = document.getElementsByName('to');
+		for(i = 0; i < to.length; i++)
+			if(to[i].type.toLowerCase() == 'text')
+				to[i].disabled = flag;
+
+		return false;
+	}
+	</script>
+	<a class="search-page-params" href="#" onclick="return switch_search_params()"><?echo GetMessage('CT_BSP_ADDITIONAL_PARAMS')?></a>
+	<div id="search_params" class="search-page-params" style="display:<?echo $arResult["REQUEST"]["FROM"] || $arResult["REQUEST"]["TO"]? 'block': 'none'?>">
+		<?$APPLICATION->IncludeComponent(
+			'bitrix:main.calendar',
+			'',
+			array(
+				'SHOW_INPUT' => 'Y',
+				'INPUT_NAME' => 'from',
+				'INPUT_VALUE' => $arResult["REQUEST"]["~FROM"],
+				'INPUT_NAME_FINISH' => 'to',
+				'INPUT_VALUE_FINISH' =>$arResult["REQUEST"]["~TO"],
+				'INPUT_ADDITIONAL_ATTR' => 'size="10"',
+			),
+			null,
+			array('HIDE_ICONS' => 'Y')
+		);?>
+	</div>
+<?endif?>
+</form>
+
+<?if(isset($arResult["REQUEST"]["ORIGINAL_QUERY"])):
+	?>
+	<div class="search-language-guess">
+		<?echo GetMessage("CT_BSP_KEYBOARD_WARNING", array("#query#"=>'<a href="'.$arResult["ORIGINAL_QUERY_URL"].'">'.$arResult["REQUEST"]["ORIGINAL_QUERY"].'</a>'))?>
+	</div><br /><?
+endif;?>
+
+<?if($arResult["REQUEST"]["QUERY"] === false && $arResult["REQUEST"]["TAGS"] === false):?>
+<?elseif($arResult["ERROR_CODE"]!=0):?>
+	<p><?=GetMessage("SEARCH_ERROR")?></p>
+	<?ShowError($arResult["ERROR_TEXT"]);?>
+
+<?elseif(count($arResult["SEARCH"])>0):?>
+	<?if($arParams["DISPLAY_TOP_PAGER"] != "N") echo $arResult["NAV_STRING"]?>
+
+    <?foreach($arResult["SEARCH"] as $arItem):?>
+        <h1 class="post_title"><a href="<?=$arItem["URL"]?>"><?=$arItem["TITLE_FORMATED"]?></a></h1>
+        <p class="post_date_2"><?=formatBDate($arItem["DATE_CHANGE"])?></p>
+        <div id="text"><?=$arItem["BODY_FORMATED"]?></div>
+    <?endforeach;?>
+
+	<?if($arParams["DISPLAY_BOTTOM_PAGER"] != "N") echo $arResult["NAV_STRING"]?>
+
+<?else:?>
+	<?ShowNote(GetMessage("SEARCH_NOTHING_TO_FOUND"));?>
+<?endif;?>
