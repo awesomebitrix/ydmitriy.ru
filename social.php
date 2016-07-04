31046d8f70f3decdb744dc61377d958c185b6611
Dmitry Yazykov <pro.d1mas91@gmail.com>
2016-07-04 18:14:56 +0300

    Разноцветвное выделение ссылок на соцсети

--- social.php
+++ social.php
@@ -1,5 +1,5 @@
 Меня также можно найти в соцсетях:
-<a href="https://github.com/D1maS91" target="_blank">GitHub</a>,
-<a href="http://vk.com/d1mas91" target="_blank">ВКонтакте</a>,
-<a href="http://instagram.com/d1mas91" target="_blank">Instagram</a> и
-<a href="https://twitter.com/D1maS91" target="_blank">Twitter</a>
+<a href="https://github.com/D1maS91" target="_blank" class="github">GitHub</a>,
+<a href="http://vk.com/d1mas91" target="_blank" class="vk">ВКонтакте</a>,
+<a href="http://instagram.com/d1mas91" target="_blank" class="instagram">Instagram</a> и
+<a href="https://twitter.com/D1maS91" target="_blank" class="twitter">Twitter</a>
