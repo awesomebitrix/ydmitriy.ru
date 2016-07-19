6b6b3455dd315b9d26f27f914c0d28622c02c739
Dmitry Yazykov <work@ydmitriy.ru>
2016-07-19 15:37:24 +0300

    Обновлена контактная информация

--- contacts/index.php
+++ contacts/index.php
@@ -7,12 +7,18 @@ $APPLICATION->SetTitle("Контакты");
         <h1 itemprop="name">Языков Дмитрий</h1>
     </div>
     <noindex>
-        <h2>Контактная информация</h2>
+        <h1>Контактная информация</h1>
     </noindex>
-    <b>Телефон: </b><span itemprop="telephone">+7 (951) 069-52-39 </span><br>
+    Связаться со мной: <br><br>
+    <b>Телефон: </b><span itemprop="telephone">+7 (951) 069-52-39 </span> <i>(с 9 до 18)</i><br><br>
     <b>Email: </b><span itemprop="email"><a href="mailto:work@ydmitriy.ru">work@ydmitriy.ru</a></span><br>
     <b>Skype: </b><a href='skype:d1mas91'>d1mas91</a><br><br>
-    <b>GitHub: </b><a href="http://github.com/d1mas91">github.com/d1mas91</a><br>
-    <b>ВКонтакте: </b><a href="http://vk.com/d1mas91">vk.com/d1mas91</a><br>
+
+    Или если вам просто интересно кто я такой и чем увлекаюсь:<br><br>
+    <b>GitHub: </b><a href="http://github.com/d1mas91" target="_blank">github.com/d1mas91</a><br>
+    <b>ВКонтакте: </b><a href="http://vk.com/d1mas91" target="_blank">vk.com/d1mas91</a><br>
+    <b>LinkedIn: </b><a href="https://www.linkedin.com/in/дмитрий-языков-a170b057" target="_blank">linkedin.com/...</a><br>
+    <b>Instagram: </b><a href="http://instagram.com/d1mas91" target="_blank">instagram.com/d1mas91</a><br>
+    <b>Twitter: </b><a href="https://twitter.com/D1maS91" target="_blank">twitter.com/d1mas91</a><br>
 </div>
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
