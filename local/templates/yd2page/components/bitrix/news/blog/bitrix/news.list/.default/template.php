d9a2d4a6dcbcc2f5d7c4ffe6bbb642b04102b0cc
Yazykov <work@ydmitriy.ru>
2017-01-19 14:07:30 +0300

    Исправлена ошибка с поддержкой Эрмитажа к разделе Блог

--- local/templates/yd2page/components/bitrix/news/blog/bitrix/news.list/.default/template.php
+++ local/templates/yd2page/components/bitrix/news/blog/bitrix/news.list/.default/template.php
@@ -8,7 +8,7 @@ $this->setFrameMode(true);
             $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
             $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
         ?>
-    <div class="topic">
+    <div class="topic" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
         <h2><a href="<?=$arItem["DETAIL_PAGE_URL"]?>"><?=$arItem["NAME"]?></a></h2>
         <p class="post_date_2"><?=formatBDate($arItem["DATE_CREATE"])?></p>
         <img
