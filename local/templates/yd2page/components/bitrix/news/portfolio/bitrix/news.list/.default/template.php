188994ce1e57c2078d595429db478f65a1209e96
Yazykov <work@ydmitriy.ru>
2017-01-20 09:22:47 +0300

    Убрана лишняя верстка news.list раздела Портфолио

--- local/templates/yd2page/components/bitrix/news/portfolio/bitrix/news.list/.default/template.php
+++ local/templates/yd2page/components/bitrix/news/portfolio/bitrix/news.list/.default/template.php
@@ -7,24 +7,24 @@ $this->setFrameMode(true);
     $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
     $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
     ?>
-        <div id="<?=$this->GetEditAreaId($arItem['ID']);?>" class="text_block">
-            <div>
-                <h2>
-                    <a href="<?=$arItem["DETAIL_PAGE_URL"]?>"><?=$arItem["NAME"]?></a>
-                </h2>
-            </div>
-            <div>
-                <img
-                    border="0"
-                    data-original="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"
-                    width="<?=$arItem["PREVIEW_PICTURE"]["WIDTH"]?>"
-                    height="<?=$arItem["PREVIEW_PICTURE"]["HEIGHT"]?>"
-                    alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>"
-                    title="<?=$arItem["PREVIEW_PICTURE"]["TITLE"]?>"
-                    />
-            </div>
-        </div>
+    <div id="<?=$this->GetEditAreaId($arItem['ID']);?>" class="text_block">
+
+        <h2>
+            <a href="<?=$arItem["DETAIL_PAGE_URL"]?>"><?=$arItem["NAME"]?></a>
+        </h2>
+
+        <img
+            border="0"
+            data-original="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"
+            width="<?=$arItem["PREVIEW_PICTURE"]["WIDTH"]?>"
+            height="<?=$arItem["PREVIEW_PICTURE"]["HEIGHT"]?>"
+            alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>"
+            title="<?=$arItem["PREVIEW_PICTURE"]["TITLE"]?>"
+        />
+
+    </div>
 <?endforeach;?>
+
 <?if($arParams["DISPLAY_BOTTOM_PAGER"]):?>
     <br /><?=$arResult["NAV_STRING"]?>
 <?endif;?>
