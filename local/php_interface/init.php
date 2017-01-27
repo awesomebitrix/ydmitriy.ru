651a180864ebab4a3efa0f735c3e88f459f30a33
Yazykov <work@ydmitriy.ru>
2017-01-27 14:15:47 +0300

    Простой SafeMode для аварийного отключения дополнительных классов

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,6 +1,19 @@
 <?
-//  Класс вспомагательных функций
-if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/classes/Helpers.php'))
-    require_once $_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/classes/Helpers.php';
+//  Реализуем SafeMode для отключения всех сторонних файлов в случае необходиости
+if ($_GET['SAFE_MODE'] == 'Y') {
+    $_SESSION['SAFE_MODE'] = 'Y';
+} elseif ($_GET['SAFE_MODE'] == 'N') {
+    unset($_SESSION['SAFE_MODE']);
+}
+
+if ($_SESSION['SAFE_MODE'] !== 'Y') {
+
+    //  Класс вспомагательных функций
+    if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/classes/Helpers.php'))
+        require_once $_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/classes/Helpers.php';
+
+}
+
+
 
 
