1db25e8196cf78e53186d8e072f244da8e207116
Yazykov <work@ydmitriy.ru>
2017-02-08 11:52:34 +0300

    Обновленный раздел Портфолио. Стили вынесены в style.css шаблона компонента

--- local/templates/yd2page/components/bitrix/news/portfolio/bitrix/news.list/.default/template.php
+++ local/templates/yd2page/components/bitrix/news/portfolio/bitrix/news.list/.default/template.php
@@ -1,29 +1,45 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 $this->setFrameMode(true);
-//dump($arResult);
+//Helpers::dump($arResult);
 ?>
-<?foreach($arResult["ITEMS"] as $arItem):?>
-    <?
-    $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
-    $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
-    ?>
-    <div id="<?=$this->GetEditAreaId($arItem['ID']);?>" class="text_block">
 
-        <h2>
-            <a href="<?=$arItem["DETAIL_PAGE_URL"]?>"><?=$arItem["NAME"]?></a>
-        </h2>
+<div class="portfolio">
 
-        <img
-            border="0"
-            data-original="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"
-            width="<?=$arItem["PREVIEW_PICTURE"]["WIDTH"]?>"
-            height="<?=$arItem["PREVIEW_PICTURE"]["HEIGHT"]?>"
-            alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>"
-            title="<?=$arItem["PREVIEW_PICTURE"]["TITLE"]?>"
-        />
+    <?foreach($arResult["ITEMS"] as $arItem):?>
 
-    </div>
-<?endforeach;?>
+        <?
+        $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+        $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+        ?>
+        <div id="<?=$this->GetEditAreaId($arItem['ID']);?>" class="portfolio_block">
+
+            <a href="<?=$arItem["DETAIL_PAGE_URL"]?>">
+
+                <div class="portfolio_text">
+                    <div class="frame">
+                        <h2>
+                            <?=$arItem["NAME"]?>
+                        </h2>
+                    </div>
+                </div>
+                <div class="portfolio_image">
+                    <img
+                        border="0"
+                        data-original="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"
+                        width="<?=$arItem["PREVIEW_PICTURE"]["WIDTH"]?>"
+                        height="<?=$arItem["PREVIEW_PICTURE"]["HEIGHT"]?>"
+                        alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>"
+                        title="<?=$arItem["PREVIEW_PICTURE"]["TITLE"]?>"
+                    />
+                </div>
+
+            </a>
+
+        </div>
+
+    <?endforeach;?>
+
+</div>
 
 <?if($arParams["DISPLAY_BOTTOM_PAGER"]):?>
     <br /><?=$arResult["NAV_STRING"]?>
