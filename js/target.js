1ab51f4aab55b972b641c62a2aba899a6172b012
Yazykov <work@ydmitriy.ru>
2017-03-25 09:15:06 +0300

    Добавлен js с кодом отслеживания целей и цель нажатия на меню

---
+++ js/target.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Yazykov Dmitry on 25.03.17.
+ * Вызов js целей
+ */
+
+$(document).ready(function(){
+    //  Событие нажатия на меню (ID 29914194)
+    $('span.button').click(function(){
+
+        yaCounter26537544.reachGoal('menu_target');
+
+    });
+});
