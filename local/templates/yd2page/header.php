fdf42812b32cfed22def7871e0bd11ca0ebf3a32
Yazykov <work@ydmitriy.ru>
2017-04-04 11:01:19 +0300

    Решена проблема с фавиконом на внутренних страницах

--- local/templates/yd2page/header.php
+++ local/templates/yd2page/header.php
@@ -18,7 +18,7 @@
     <!-- CSS -->
     <?$APPLICATION->ShowCSS();?>
     <!-- !CSS -->
-    <link rel="icon" href="favicon.ico" type="image/vnd.microsoft.icon"/>
+    <link rel="icon" href="/favicon.ico" type="image/vnd.microsoft.icon"/>
 
     <?
         //  Фикс множества багов "Uncaught ReferenceError: BXHotKeys is not defined" для админа
