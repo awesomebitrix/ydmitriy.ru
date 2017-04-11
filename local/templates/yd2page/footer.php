5a2b68ff08fc11d6e71b61f0dd8cd0e2edd89760
Yazykov <work@ydmitriy.ru>
2017-04-11 09:56:51 +0300

    Исправлена проблема с подключением удаленных скриптов lazyload

--- local/templates/yd2page/footer.php
+++ local/templates/yd2page/footer.php
@@ -4,11 +4,8 @@
     use Bitrix\Main\Page\Asset;
 
     Asset::getInstance()->addJs('/bower_components/jquery/dist/jquery.min.js');
-    Asset::getInstance()->addJs('/bower_components/jquery.lazyload/jquery.lazyload.js');
     Asset::getInstance()->addJs('https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js?skin=desert');
-    Asset::getInstance()->addJs('/js/lazyload.js');
     Asset::getInstance()->addJs('/js/min/all.min.js');
-
 ?>
 
 <?$APPLICATION->IncludeComponent(
